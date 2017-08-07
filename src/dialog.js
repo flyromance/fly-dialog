97a0f9a364c058d7e9414922aeabd3d802ed1bbd
flyromance <814885004@qq.com>
2017-08-07 22:05:14 +0800

    fix

--- src/dialog.js
+++ src/dialog.js
@@ -3,7 +3,7 @@ require('./style/dialog.css');
 var $ = require('jquery');
 var Popup = require('./script/popup');
 var defaults = require('./script/config');
-console.log(123);
+
 var _count = 0;
 var _expando = new Date() - 0; // Date.now()
 var _isIE6 = !('minWidth' in $('html')[0].style);
@@ -118,7 +118,6 @@ artDialog.create = function(options) {
     if (options.zIndex) {
         Popup.zIndex = options.zIndex;
     }
-
 
     // 设置 ARIA 信息
     $popup.attr({
